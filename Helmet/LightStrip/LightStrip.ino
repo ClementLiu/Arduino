e6f36b5f607239684a3330fda97e28f8a3421f8b
ClementLiu <ziwei.liu@cca.edu>
2017-10-28 17:15:15 -0700

    Remove DS

--- Helmet/LightStrip/LightStrip.ino
+++ Helmet/LightStrip/LightStrip.ino
@@ -10,7 +10,7 @@
 #include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
 //#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
 
-#define NUMPIXELS 60 // Number of LEDs in strip
+#define NUMPIXELS 67 // Number of LEDs in strip
 
 // Here's how to control the LEDs from any two pins:
 #define DATAPIN    4
